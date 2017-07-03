add549adaffaaa9757f939d6f259f6562e354e0e
KenigGA <kenigsbergerga@gmail.com>
2017-07-03 18:10:10 +0300

    [MAJOR] add stack.c

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 all: stack
 
-stack: main.o 
-	gcc main.o -o stack
+stack: main.o stack.o
+	gcc main.o stack.o -o stack
 
 main.o: main.c
 	gcc -c main.c
+
+stack.o: stack.c
+	gcc -c stack.c
 
 clean:
 	rm -rf *.o stack
--- stack.h
+++ stack.h
@@ -1,8 +1,7 @@
-#define STACK_OVERFLOW  -100
-#define STACK_UNDERFLOW -101
-#define OUT_OF_MEMORY   -102
-#define INIT_SIZE 10 /* starting stack size */
-#define ADD_SPACE 1 /* shows by how many elements the stack is increasing after resize function */
+#ifndef STACK
+#define STACK
+#include <stdio.h>
+#include <stdlib.h>
 
 /*
  * description of structure "my_stack"
@@ -17,77 +16,30 @@ typedef struct my_stack
 /*
  * function which create stack, returns the pointer to the stack, which was created
 */
-my_stack* create_stack() 
-{
-	my_stack* new_stack = NULL; /* new stack pointer is equal to NULL */
-	new_stack = malloc(sizeof(my_stack)); /* allocating memory for the new stack */
-	if (new_stack == NULL) 
-		exit(OUT_OF_MEMORY);
-	new_stack->size = INIT_SIZE; /* the size of new stack is equal to constant INIT_SIZE */
-	new_stack->data = malloc(new_stack->size * sizeof(int)); /* allocating memory to the stack field "data" */
-	if (new_stack->data == NULL) 
-	{
-		free(new_stack);
-		exit(OUT_OF_MEMORY);
-	}
-	new_stack->top = 0; 
-	return new_stack;
-}
+my_stack* create_stack(); 
 
 /*
  * function which delete the stack, takes pointer to pointer to stack 
 */ 
-void delete_stack(my_stack** stack) 
-{
-	free((*stack)->data);
-	free(*stack);
-	*stack = NULL;
-}
+void delete_stack(my_stack** stack);
 
 /*
  * function which resize the stack, takes pointer to stack 
 */ 
-void resize(my_stack* stack)
-{
-	stack->size += ADD_SPACE;
-	stack->data = realloc(stack->data, stack->size * sizeof(int));
-	if (stack->data == NULL) 
-		exit(STACK_OVERFLOW);
-}
+void resize(my_stack* stack);
 
 /*
  * put the element "value" on top of the stack, takes pointer to stack and "value"
 */
-void push(my_stack* stack, int value) 
-{
-	if (stack->top >= stack->size) /* checks is it necessary to resize a stack */
-		resize(stack);	       	
-	stack->data[stack->top] = value;
-	stack->top++;
-}
+void push(my_stack* stack, int value); 
 
 /*
  * pops element from the top of the stack and returns it
 */
-int pop(my_stack* stack) 
-{
-	if (stack->top == 0) 
-		exit(STACK_UNDERFLOW);
-	stack->top--;
-	return stack->data[stack->top];
-}
+int pop(my_stack* stack);
 
 /*
- * return element from the top os the stack
+ * return element from the top of the stack
 */
-int peek(const my_stack* stack) 
-{
-	if (stack->top <= 0) 
-		exit(STACK_UNDERFLOW);
-	return stack->data[stack->top - 1];
-}
-
-
-
-
-
+int peek(const my_stack* stack); 
+#endif
